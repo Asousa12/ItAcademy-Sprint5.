6f9e07a9fe3bea0a93bc7c801f82ddaf39855e35
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-04-30 17:42:17 +0200

    Delete S05.T2.N1.F1SousaAndreia directory

--- S05.T2.N1.F1SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/t52/n1/f1/t52/n1/f1SousaAndreia/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.t52.n1.f1.t52.n1.f1SousaAndreia.exception;
-
-public class InvalidInputException extends Throwable {
-    public InvalidInputException(String datosDeJuegoInvalidos) {
-        super(datosDeJuegoInvalidos);
-    }
-}
