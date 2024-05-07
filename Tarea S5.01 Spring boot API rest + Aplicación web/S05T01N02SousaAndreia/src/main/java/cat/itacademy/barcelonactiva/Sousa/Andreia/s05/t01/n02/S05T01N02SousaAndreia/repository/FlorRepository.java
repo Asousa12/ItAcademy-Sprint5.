95cacd805c5628538365cd8a5bf44edfb4e7663a
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-05-07 21:31:05 +0200

    Delete Tarea S5.01 Spring boot API rest + Aplicación web directory

--- Tarea S5.01 Spring boot API rest + Aplicación web/S05T01N02SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/s05/t01/n02/S05T01N02SousaAndreia/repository/FlorRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.s05.t01.n02.S05T01N02SousaAndreia.repository;
-
-import cat.itacademy.barcelonactiva.Sousa.Andreia.s05.t01.n02.S05T01N02SousaAndreia.domain.FlorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlorRepository extends JpaRepository<FlorEntity, Integer> {
-}
