54428bf670b9299f18e81a8619ab0b59147db8e6
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-05-07 21:30:37 +0200

    Delete S05.T2.N1.F1SousaAndreia directory

--- S05.T2.N1.F1SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/t52/n1/f1/t52/n1/f1SousaAndreia/exception/GameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.t52.n1.f1.t52.n1.f1SousaAndreia.exception;
-
-public class GameAlreadyExistsException extends Exception {
-    public GameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- S05.T2.N1.F1SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/t52/n1/f1/t52/n1/f1SousaAndreia/exception/PlayerNameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.t52.n1.f1.t52.n1.f1SousaAndreia.exception;
-
-public class PlayerNameAlreadyExistsException extends RuntimeException {
-    public PlayerNameAlreadyExistsException(String s) {
-        super(s);
-    }
-}
-
