a0443ac5b00ee956e69b3d109c8b90faaa4f8f50
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-05-07 21:30:51 +0200

    Delete S05T2N1F3SousaAndreia directory

--- S05T2N1F3SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/S05/T2/N1/F3/S05T2N1F3SousaAndreia/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.S05.T2.N1.F3.S05T2N1F3SousaAndreia.exception;
-
-public class InvalidInputException extends Throwable {
-    public InvalidInputException(String datosDeJuegoInvalidos) {
-        super(datosDeJuegoInvalidos);
-    }
-}
--- S05T2N1F3SousaAndreia/src/main/java/cat/itacademy/barcelonactiva/Sousa/Andreia/S05/T2/N1/F3/S05T2N1F3SousaAndreia/exception/UserAlreadyExistsException.java
+++
@@ -1,6 +0,0 @@
-package cat.itacademy.barcelonactiva.Sousa.Andreia.S05.T2.N1.F3.S05T2N1F3SousaAndreia.exception;
-
-public class UserAlreadyExistsException extends Throwable {
-    public UserAlreadyExistsException(String s) {
-    }
-}
